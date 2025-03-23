7fa4b6e82b440c15f10eacb9d2cfa48bb55d7555
Karl-Gilmartin <karl_gilmartin@hotmail.com>
2025-03-23 13:28:43 +0000

    botton nav bar

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const BottomNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.white,
+      elevation: 10,
+      selectedItemColor: const Color(0xFF1F8DED),
+      unselectedItemColor: Colors.grey.shade500,
+      selectedFontSize: 14,
+      unselectedFontSize: 12,
+      iconSize: 26,
+      currentIndex: selectedIndex,
+      onTap: onItemTapped,
+      showUnselectedLabels: true,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.report_outlined),
+          activeIcon: Icon(Icons.report),
+          label: 'Report',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          activeIcon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
